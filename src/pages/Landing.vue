f32a23292774dfe9d3c0c2f2337edbf866c896c0
mar7in91 <martinandres.perez@gmail.com>
2021-07-11 02:33:05 -0300

    Primer estructura de la landing

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -3,36 +3,28 @@
     <div class="page-header page-header-small">
       <parallax
         class="page-header-image"
-        style="background-image: url('img/bg6.jpg')"
+        style="background-image: url('img/daniel-dinuzzo-qCjolcMFaLI-unsplash.jpg')"
       >
       </parallax>
       <div class="content-center">
         <div class="container">
-          <h1 class="title">This is our great company.</h1>
-          <div class="text-center">
-            <a href="#pablo" class="btn btn-primary btn-icon btn-round">
-              <i class="fab fa-facebook-square"></i>
-            </a>
-            <a href="#pablo" class="btn btn-primary btn-icon btn-round">
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a href="#pablo" class="btn btn-primary btn-icon btn-round">
-              <i class="fab fa-google-plus"></i>
-            </a>
-          </div>
+          <h1 class="title">Te invitamos a conocernos aún más</h1>
         </div>
       </div>
     </div>
-    <div class="section section-about-us">
+    <landing-design-section></landing-design-section>
+    <landing-documentation-section></landing-documentation-section>
+    <landing-visualization-section></landing-visualization-section>
+
+    <div class="section section-about-us" data-background-color="black">
       <div class="container">
         <div class="row">
           <div class="col-md-8 ml-auto mr-auto text-center">
-            <h2 class="title">Who we are?</h2>
+            <h2 class="title">¿QUIÉNES SOMOS?</h2>
             <h5 class="description">
-              According to the National Oceanic and Atmospheric Administration,
-              Ted, Scambos, NSIDClead scentist, puts the potentially record low
-              maximum sea ice extent tihs year down to low ice extent in the
-              Pacific and a late drop in ice extent in the Barents Sea.
+              ADM Estudio nació en 2016, brindando servicios de dibujo de planos para arquitectos,
+              constructoras y estudios. Con el paso del tiempo y las distintas capacitaciones comenzamos a
+              implementar la visualización para proyectos y diseño.
             </h5>
           </div>
         </div>
@@ -42,76 +34,59 @@
             <div class="col-md-6">
               <div
                 class="image-container image-left"
-                style="background-image: url('img/login.jpg')"
+                style="background-image: url('img/obra_01.jpg')"
               >
                 <!-- First image on the left side -->
                 <p class="blockquote blockquote-primary">
-                  "Over the span of the satellite record, Arctic sea ice has
-                  been declining significantly, while sea ice in the
-                  Antarctichas increased very slightly"
+                  "Somos creativos y proactivos, nos esforzamos al máximo para aplicarlo en cada proyecto y
+                  relación con el cliente. Nuestro desafío es potenciar y crear valor agregado en los futuros
+                  proyectos de arquitectura."
                   <br />
                   <br />
-                  <small>-NOAA</small>
+                  <small>Alan Di Mauro</small>
                 </p>
               </div>
               <!-- Second image on the left side of the article -->
-              <div
-                class="image-container"
-                style="background-image: url('img/bg3.jpg')"
-              ></div>
+
             </div>
             <div class="col-md-5">
               <!-- First image on the right side, above the article -->
               <div
                 class="image-container image-right"
-                style="background-image: url('img/bg1.jpg')"
+                style="background-image: url('img/obra_02.jpg')"
               ></div>
               <h3>
-                So what does the new record for the lowest level of winter ice
-                actually mean
+                Somos un equipo integrado por Arquitectos y profesionales de la construcción,
+                con experiencia en el rubro.
               </h3>
               <p>
-                The Arctic Ocean freezes every winter and much of the sea-ice
-                then thaws every summer, and that process will continue whatever
-                happens with climate change. Even if the Arctic continues to be
-                one of the fastest-warming regions of the world, it will always
-                be plunged into bitterly cold polar dark every winter. And
-                year-by-year, for all kinds of natural reasons, there’s huge
-                variety of the state of the ice.
+                Nuestro servicio es 100% personalizado y adaptable a
+                cualquier tipo de proyecto.
+                Realizamos encargos de visualización para terceros, como arquitectos, diseñadores de
+                interiores, particulares e inmobiliarias, como también proyectos de obra nueva y remodelaciones
+                con nuestro diseño.
               </p>
               <p>
-                For a start, it does not automatically follow that a record
-                amount of ice will melt this summer. More important for
-                determining the size of the annual thaw is the state of the
-                weather as the midnight sun approaches and temperatures rise.
-                But over the more than 30 years of satellite records, scientists
-                have observed a clear pattern of decline, decade-by-decade.
-              </p>
-              <p>
-                The Arctic Ocean freezes every winter and much of the sea-ice
-                then thaws every summer, and that process will continue whatever
-                happens with climate change. Even if the Arctic continues to be
-                one of the fastest-warming regions of the world, it will always
-                be plunged into bitterly cold polar dark every winter. And
-                year-by-year, for all kinds of natural reasons, there’s huge
-                variety of the state of the ice.
+                Somos creativos y proactivos, nos esforzamos al máximo para aplicarlo en cada proyecto y
+                relación con el cliente. Nuestro desafío es potenciar y crear valor agregado en los futuros
+                proyectos de arquitectura.
               </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="section section-team text-center">
+    <div class="section section-team text-center" >
       <div class="container">
-        <h2 class="title">Here is our team</h2>
+        <h2 class="title">EL EQUIPO</h2>
         <div class="team">
-          <div class="row">
+          <div class="row" style="justify-content: center;">
             <div class="col-md-4">
               <div class="team-player">
                 <img
-                  src="img/avatar.jpg"
-                  alt="Thumbnail Image"
-                  class="rounded-circle img-fluid img-raised"
+                    src="img/avatar.jpg"
+                    alt="Thumbnail Image"
+                    class="rounded-circle img-fluid img-raised"
                 />
                 <h4 class="title">Romina Hadid</h4>
                 <p class="category text-primary">Model</p>
@@ -122,116 +97,39 @@
                   them outside the site.
                 </p>
                 <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-twitter"></i
+                ><i class="fab fa-twitter"></i
                 ></a>
                 <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-instagram"></i
+                ><i class="fab fa-instagram"></i
                 ></a>
                 <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-facebook-square"></i
+                ><i class="fab fa-facebook-square"></i
                 ></a>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="team-player">
-                <img
-                  src="img/ryan.jpg"
-                  alt="Thumbnail Image"
-                  class="rounded-circle img-fluid img-raised"
-                />
-                <h4 class="title">Ryan Tompson</h4>
-                <p class="category text-primary">Designer</p>
-                <p class="description">
-                  You can write here details about one of your team members. You
-                  can give more details about what they do. Feel free to add
-                  some <a href="#">links</a> for people to be able to follow
-                  them outside the site.
-                </p>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-twitter"></i
-                ></a>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-linkedin"></i
-                ></a>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="team-player">
-                <img
-                  src="img/eva.jpg"
-                  alt="Thumbnail Image"
-                  class="rounded-circle img-fluid img-raised"
-                />
-                <h4 class="title">Eva Jenner</h4>
-                <p class="category text-primary">Fashion</p>
-                <p class="description">
-                  You can write here details about one of your team members. You
-                  can give more details about what they do. Feel free to add
-                  some <a href="#">links</a> for people to be able to follow
-                  them outside the site.
-                </p>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-google-plus"></i
-                ></a>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-youtube"></i
-                ></a>
-                <a href="#pablo" class="btn btn-primary btn-icon btn-round"
-                  ><i class="fab fa-twitter"></i
-                ></a>
-              </div>
-            </div>
+
           </div>
         </div>
       </div>
     </div>
-    <div class="section section-contact-us text-center">
-      <div class="container">
-        <h2 class="title">Want to work with us?</h2>
-        <p class="description">Your project is very important to us.</p>
-        <div class="row">
-          <div class="col-lg-6 text-center ml-auto mr-auto col-md-8">
-            <fg-input
-              class="input-lg"
-              placeholder="First Name..."
-              v-model="form.firstName"
-              addon-left-icon="now-ui-icons users_circle-08"
-            >
-            </fg-input>
-            <fg-input
-              class="input-lg"
-              placeholder="Email Here..."
-              v-model="form.email"
-              addon-left-icon="now-ui-icons ui-1_email-85"
-            >
-            </fg-input>
-            <div class="textarea-container">
-              <textarea
-                class="form-control"
-                name="name"
-                rows="4"
-                cols="80"
-                v-model="form.message"
-                placeholder="Type a message..."
-              ></textarea>
-            </div>
-            <div class="send-button">
-              <n-button type="primary" round block size="lg"
-                >Send Message</n-button
-              >
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+
+    <signup-form></signup-form>
   </div>
 </template>
 <script>
 import { Button, FormGroupInput } from '@/components';
+import LandingDesignSection from "@/pages/components/LandingDesignSection";
+import LandingDocumentationSection from "@/pages/components/LandingDocumentationSection";
+import LandingVisualizationSection from "@/pages/components/LandingVisualizationSection";
+import SignupForm from "@/pages/components/SignupForm";
 export default {
   name: 'landing',
   bodyClass: 'landing-page',
   components: {
+    SignupForm,
+    LandingVisualizationSection,
+    LandingDocumentationSection,
+    LandingDesignSection,
     [Button.name]: Button,
     [FormGroupInput.name]: FormGroupInput
   },
---
+++ src/pages/components/LandingDesignSection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="section section-nucleo-icons">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-12">
+          <h2 class="title">DISEÑO</h2>
+          <h5 class="description">
+            dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam          </h5>
+          <!-- <div class="nucleo-container">
+                    <img v-lazy="'assetsimg/nucleo.svg'" alt="">
+                </div> -->
+        </div>
+        <div class="col-lg-6 col-md-12">
+          <img src="img/ampliacion.jpg" alt="obra_plano" style="inline-size: -webkit-fill-available;">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
